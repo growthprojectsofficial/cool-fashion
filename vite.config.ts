55abc1ba9d7f23eafd766d601f365a05bd964e16
growthprojectsofficial <snapdragon9@gmail.com>
2026-06-11 22:43:20 +0100

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import path from "path"
-import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   base: "/cool-fashion/",
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
